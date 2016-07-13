936994fe8b6494a1d764f91c3c552e9847101f7f
Lambert Boskamp <lambert@boskamp-consulting.com>
2016-07-13 22:13:47 +0200

    Fix typo in JSDoc

--- src/main/javascript/fx_bobj_nextGroupEntry.js
+++ src/main/javascript/fx_bobj_nextGroupEntry.js
@@ -1,7 +1,7 @@
 /**
  * Retrieves next BOBJ group, if any, during initial load.
  * @see fx_bobj_EntryReader.read
- * @returns {com.sap.idm.ic.DSEEntry?} - IDM entry or null if no more aliases
+ * @returns {com.sap.idm.ic.DSEEntry?} - IDM entry or null if no more groups
  * @requires fx_bobj_EntryReader
  */
 function fx_bobj_nextGroupEntry()
